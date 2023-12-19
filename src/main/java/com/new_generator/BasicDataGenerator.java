bcc247692d295d6395f2a99b61e34663ab5c17bf
karolxd9 <karolp05082002@gmail.com>
2023-12-19 15:54:56 +0100

    Next step of refactoring our code

--- src/main/java/com/new_generator/BasicDataGenerator.java
+++ src/main/java/com/new_generator/BasicDataGenerator.java
@@ -36,7 +36,14 @@ public abstract class BasicDataGenerator<V,T> {
         return null;
     }
 
-    public boolean checkUnique(String table,String column,String value){
+    /**
+     * Sprawdza czy dana wartość pojawia się w relacji
+     * @param table Nazwa tabeli
+     * @param column Nazwa atrybutu
+     * @param value Wartość
+     * @return
+     */
+    protected boolean checkUnique(String table,String column,String value){
         int licznik = 0;
         String query = "SELECT "+ column + " FROM "+ table + " WHERE "+ column + " = "+ "'"+ (T)(value) + "'";
         try{
@@ -54,6 +61,30 @@ public abstract class BasicDataGenerator<V,T> {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Znajdywanie ekstremum globalnego w danej kolumnie
+     * @param isMax czy ma to być największa wartość w kolumnie
+     * @param column nazwa kolumny
+     * @param table nazwa tabeli
+     * @return wartość maksymalna(minimalna)
+     */
+    protected String getExtremeColumn(boolean isMax, String column, String table){
+        String query = "SELECT MAX("+column+")"+" FROM "+table;
+        String returnValue = "";
+        try{
+            QueryExecutor queryExecutor = new QueryExecutor();
+            ResultSet queryResult = queryExecutor.executeSelect(query);
+            queryResult.next();
+            returnValue = (String)(queryResult.getObject(column));
+        }
+        catch(SQLException | NullPointerException e){
+            e.printStackTrace();
+        }
+
+        return returnValue;
+
     }
 
 
