d9625a3d979c0147394b06fcad19f4330b7a2a42
karolxd9 <karolp05082002@gmail.com>
2024-01-03 00:03:30 +0100

    Basic login form frontend

--- src/main/java/com/example/quiz_system/HelloApplication.java
+++ src/main/java/com/example/quiz_system/HelloApplication.java
@@ -18,22 +18,19 @@ import java.util.Set;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setMaximized(true);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setMaximized(true);
+        primaryStage.setTitle("Strona główna");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) throws SQLException, InterruptedException {
         DBConnector.connect();
         SystemInfo info = new SystemInfo();
         launch();
-
-
-
     }
 
 }
